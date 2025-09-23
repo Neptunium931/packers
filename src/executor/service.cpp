d022dbbd11510f25bbc9ad0d24c679b3489b932a
neptumiun <tymothe.billerey@gmail.com>
2025-09-23 20:10:56 +0200

    style: clang-format

--- src/executor/service.cpp
+++ src/executor/service.cpp
@@ -1,8 +1,8 @@
 #include "service.hpp"
-#include <vector>
+#include "executor/executor.hpp"
 #include <string>
 #include <utility>
-#include "executor/executor.hpp"
+#include <vector>
 
 namespace packers::Executor
 {

434e77ed0ada16eda5718264bdf525b13fb2f6eb
neptumiun <tymothe.billerey@gmail.com>
2025-10-07 20:13:50 +0200

    feat(packers): add support for building executables

--- src/packers.cpp
+++ src/packers.cpp
@@ -45,11 +45,67 @@ main() -> int
                                         } /
                                         "debug");
   }
-  catch (std::filesystem::filesystem_error const &e)
+  catch (std::filesystem::filesystem_error const &exception)
   {
     std::cerr << "Failed to create build directory\n";
-    std::cerr << e.what() << '\n';
+    std::cerr << exception.what() << '\n';
     return 1;
   }
+  if (!std::filesystem::exists("src"))
+  {
+    std::cerr << "src directory does not exist\n";
+    return 1;
+  }
+  auto runner = packers::Executor::Service{};
+  auto objFiles = std::vector<std::string>{};
+  for (auto const &file : std::filesystem::directory_iterator("src"))
+  {
+    std::cout << "Processing " << file.path().filename() << '\n';
+    if (file.is_directory())
+    {
+      continue;
+    }
+    auto const fileName = file.path().filename().string();
+    if (fileName.ends_with(".cpp"))
+    {
+      runner.submit(
+        std::string{ "clang++" }
+          .append(" -std=" + package->standard)
+          .append(" -Isrc")
+          .append(" -Iinclude")
+          .append(" -Wall")
+          .append(" -Wextra")
+          .append(" -O0")
+          .append(" -g")
+          .append(" -o " + package->build.dir + "/" + fileName + ".o")
+          .append(" -c src/" + fileName));
+      objFiles.push_back(package->build.dir + "/" + fileName + ".o");
+      std::cout << "Compiled " << fileName << " to " << fileName << ".o\n";
+    }
+  }
+  runner.runAwait();
+  auto linkCommand =
+    std::string{ "clang++" }
+      .append(" -std=" + package->standard)
+      .append(" -o " + package->build.dir + "/" + package->name)
+      .append(" -Wall")
+      .append(" -Wextra")
+      .append(" -O0")
+      .append(" -g");
+  std::cout << linkCommand << '\n';
+  for (auto const &objFile : objFiles)
+  {
+    linkCommand.append(" " + objFile);
+  }
+  auto linkResult = packers::Executor::runSync(linkCommand);
+  if (linkResult.getCode() != 0)
+  {
+    std::cerr << "Failed to link executable\n";
+    std::cerr << linkResult.getError() << '\n';
+    return 1;
+  }
+  std::cout << "Successfully built executable\n";
+  std::cout << "Executable: " << package->build.dir + "/" + package->name
+            << '\n';
   return 0;
 }
